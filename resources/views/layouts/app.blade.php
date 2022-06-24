cb0230792189b597fe90b78e5dba4891e54b0f37
toto <test@test.com>
2022-06-24 20:46:10 +0300

    view add nav tasks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,7 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ route('task.index') }}">Tasks</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
